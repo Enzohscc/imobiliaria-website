2fc2fd8fb3a2727669bae77e056bafe83297a836
Sustentacao Desenvolvimento <sustentacao@brunsker.com.br>
2024-03-15 11:28:57 -0300

    config routes and create pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: async () => (await (import('./domain/pages/inicio/inicio.component'))).InicioComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
