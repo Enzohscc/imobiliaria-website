4d1b7093854df9ef93c6fc6ded2eca5edbd42abf
Sustentacao Desenvolvimento <sustentacao@brunsker.com.br>
2024-03-20 11:17:03 -0300

    add system route in pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
         loadComponent: async () => (await (import('./domain/pages/inicio/inicio.component'))).InicioComponent
     },
     {
+        path: 'imoveis',
+        loadComponent: async () => (await (import('./domain/pages/imoveis/imoveis.component'))).ImoveisComponent
+    },
+    {
         path: '**',
         redirectTo: ''
     }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,16 @@
-import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule,MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule],
+  imports: [MatIconModule, MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -19,5 +20,5 @@ export class HeaderComponent {
 
   toggleMenu() {
     this.isMenuActive = !this.isMenuActive;
-}
+  }
 }
