6c88e4a854fbca54966ac70afd3081d08320d18e
Enzohscc <99258499+Enzohscc@users.noreply.github.com>
2024-04-02 20:00:40 -0300

    fix problems and feat page contato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,23 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: '',
-        loadComponent: async () => (await (import('./domain/pages/inicio/inicio.component'))).InicioComponent
+      path: '',
+      loadComponent: async () => (await (import('./domain/pages/inicio/inicio.component'))).InicioComponent
     },
     {
-        path: 'imoveis',
-        loadComponent: async () => (await (import('./domain/pages/imoveis/imoveis.component'))).ImoveisComponent
+      path: 'imoveis',
+      loadComponent: async () => (await (import('./domain/pages/imoveis/imoveis.component'))).ImoveisComponent
     },
     {
-        path: 'sobre',
-        loadComponent: async () => (await (import('./domain/pages/sobre/sobre.component'))).SobreComponent
+      path: 'sobre',
+      loadComponent: async () => (await (import('./domain/pages/sobre/sobre.component'))).SobreComponent
     },
     {
-        path: '**',
-        redirectTo: ''
+      path: 'contato',
+      loadComponent: async () => (await (import('./domain/pages/contato/contato.component'))).ContatoComponent
+  },
+    {
+      path: '**',
+      redirectTo: ''
     }
 ];
